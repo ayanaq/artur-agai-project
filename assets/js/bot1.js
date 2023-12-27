66d9cdd671294382936ac155f0101e8eb16116d5
ayanaq <ayana.sydykova@alatoo.edu.kg>
2023-12-27 11:35:53 +0600

    final project

--- assets/js/bot1.js
+++
@@ -1,8 +0,0 @@
-const responseObj = {
-    hello: "Hey ! How are you doing ?",
-    hey: "Hey! What's Up",
-    who: "Arthur agai is the best teacher",
-    what: "Ask Arthur agai",
-    today: new Date().toDateString(),
-    time: new Date().toLocaleTimeString(),
-  };
