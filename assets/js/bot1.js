5a1aaf4b59865ce6a84e328c79e32089d84ad36f
ayanaaw <ayyyana2006@gmail.com>
2023-12-27 12:44:33 +0600

    final

--- assets/js/bot1.js
+++ assets/js/bot1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const responseObj = {
     hello: "Hey ! How are you doing ?",
     hey: "Hey! What's Up",
@@ -6,13 +5,4 @@ const responseObj = {
     what: "Ask Arthur agai",
     today: new Date().toDateString(),
     time: new Date().toLocaleTimeString(),
-=======
-const responseObj = {
-    hello: "Hey ! How are you doing ?",
-    hey: "Hey! What's Up",
-    who: "Arthur agai is the best teacher",
-    what: "Ask Arthur agai",
-    today: new Date().toDateString(),
-    time: new Date().toLocaleTimeString(),
->>>>>>> main
   };
