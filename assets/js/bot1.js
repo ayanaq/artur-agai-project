9916d4518191447668076775abbe69460471caeb
ayanaaw <ayyyana2006@gmail.com>
2023-12-27 12:29:47 +0600

    login commit

---
+++ assets/js/bot1.js
@@ -0,0 +1,8 @@
+const responseObj = {
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    who: "Arthur agai is the best teacher",
+    what: "Ask Arthur agai",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+  };
